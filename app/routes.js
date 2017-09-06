07fc8de1342ba92149026f1be7a59781ebbd9600
Merrill Lines <linesmerrill@gmail.com>
2017-09-05 19:26:08 -0700

    fix styling on login page for civilian and police

--- app/routes.js
+++ app/routes.js
@@ -5,71 +5,106 @@ var Friend = require('../app/models/friend');
 async = require("async");
 var path = require('path'),
  fs = require('fs');
+ var express = require('express')
+var app = express()
 module.exports = function(app, passport, server) {
- app.get('/', function(request, response) {
-  response.render('index.html');
- });
- app.get('/user', auth, function(request, response) {
-  response.render('user.html', {
-   user: request.user
-  });
- });
 
+app.all('*', checkUserRoute);
 
- app.get('/image.png', function(req, res) {
-  res.sendfile(path.resolve('./uploads/image_' + req.user._id));
- });
-
- app.get('/edit', auth, function(request, response) {
-  response.render('edit.html', {
-   user: request.user
-  });
- });
-
- app.get('/civ-dashboard', auth, function(request, response) {
-  Civilian.find({
-   'civilian.username': request.user.user.username
-  }, function(err, dbPersonas) {
-   Vehicle.find({
-    'vehicle.username': request.user.user.username
-   }, function(err, dbVehicles) {
-
-
-    response.render('civ-dashboard.html', {
-     user: request.user,
-     personas: dbPersonas,
-     vehicles: dbVehicles
-    });
-   });
-  })
- });
-
- app.get('/police-dashboard', auth, function(request, response) {
-   Civilian.find({}, function(err, dbCivilians) {
-     Vehicle.find({}, function(err, dbVehicles) {
-      response.render('police-dashboard.html', {
-       user: request.user,
-       civilians: dbCivilians,
-       vehicles: dbVehicles
+function checkUserRoute(req, res, next) {
+  switch (req.path) {
+    case '/':
+      app.get('/', function(request, response) {
+        response.render('index.html');
       });
-    });
-  });
- });
-
- app.get('/logout', function(request, response) {
-  request.logout();
-  response.redirect('/');
- });
-
- app.get('/login', function(req, res) {
-  res.redirect('/');
- })
-
- app.get('/login-civ', function(request, response) {
-  response.render('login-civ.html', {
-   message: request.flash('error')
-  });
- });
+      next();
+      break;
+    case '/edit':
+      app.get('/edit', auth, function(request, response) {
+        response.render('edit.html', {
+         user: request.user
+        });
+      });
+      next();
+      break;
+    case '/login-civ':
+      app.get('/login-civ', function(request, response) {
+        response.render('login-civ.html', {
+         message: request.flash('error')
+        });
+      });
+      next();
+      break;
+    case '/login-police':
+      app.get('/login-police', function(request, response) {
+        response.render('login-police.html', {
+          message: request.flash('error')
+        });
+      });
+      next();
+      break;
+    case '/signup-civ':
+      app.get('/signup-civ', function(request, response) {
+        response.render('signup-civ.html', {
+          message: request.flash('signuperror')
+        });
+      });
+      next();
+      break;
+    case '/signup-police':
+      app.get('/signup-police', function(request, response) {
+        response.render('signup-police.html', {
+          message: request.flash('signuperror')
+        });
+      });
+      next();
+      break;
+    case '/logout':
+      app.get('/logout', function(request, response) {
+        request.logout();
+        response.redirect('/');
+      });
+      next();
+      break;
+    case '/civ-dashboard':
+      app.get('/civ-dashboard', auth, function(request, response) {
+       Civilian.find({
+        'civilian.username': request.user.user.username
+       }, function(err, dbPersonas) {
+        Vehicle.find({
+         'vehicle.username': request.user.user.username
+        }, function(err, dbVehicles) {
+         response.render('civ-dashboard.html', {
+          user: request.user,
+          personas: dbPersonas,
+          vehicles: dbVehicles
+         });
+        });
+       })
+      });
+      next();
+      break;
+    case '/police-dashboard':
+      app.get('/police-dashboard', auth, function(request, response) {
+        Civilian.find({}, function(err, dbCivilians) {
+          Vehicle.find({}, function(err, dbVehicles) {
+           response.render('police-dashboard.html', {
+            user: request.user,
+            civilians: dbCivilians,
+            vehicles: dbVehicles
+           });
+         });
+       });
+      });
+      next();
+      break;
+    default:
+      app.get('*', function(request, response) {
+        response.render('page-not-found.html');
+      });
+      next();
+  }
+}
 
  app.post('/login-civ', passport.authenticate('login', {
   successRedirect: '/civ-dashboard',
@@ -77,30 +112,11 @@ module.exports = function(app, passport, server) {
   failureFlash: true
  }));
 
- app.get('/login-police', function(request, response) {
-  response.render('login-police.html', {
-   message: request.flash('error')
-  });
- });
-
  app.post('/login-police', passport.authenticate('login', {
   successRedirect: '/police-dashboard',
   failureRedirect: '/login-police',
   failureFlash: true
  }));
-
- app.get('/signup-police', function(request, response) {
-  response.render('signup-police.html', {
-   message: request.flash('signuperror')
-  });
- });
-
- app.get('/signup-civ', function(request, response) {
-  response.render('signup-civ.html', {
-   message: request.flash('signuperror')
-  });
- });
-
 
  app.post('/signup-civ', passport.authenticate('signup', {
   successRedirect: '/civ-dashboard',
@@ -113,13 +129,6 @@ module.exports = function(app, passport, server) {
   failureRedirect: '/signup-police',
   failureFlash: true
  }));
-
- app.get('/edit', function(request, response) {
-  response.render('edit.html', {
-   message: request.flash('updateerror')
-  });
- });
-
 
  app.post('/edit', function(req, res) {
   var tempPath = req.files.file.path,
@@ -168,104 +177,8 @@ module.exports = function(app, passport, server) {
    myVeh.save(function(err, fluffy) {
     if (err) return console.error(err);
    });
-
   })
  });
-
- app.get('/profile', auth, function(request, response) {
-  var query = Friend.find({
-   'friend.mainfriendid': request.user._id
-  }, {
-   'friend.anotherfriendid': 1
-  });
-  query.exec(function(err, friends) {
-
-   if (!err) {
-    var frdDetails = []
-
-    async.each(friends,
-     function(friend, callback) {
-      if (friend.friend.anotherfriendid == '') {
-       console.log('No Friend')
-      } else {
-       User.findById(friend.friend.anotherfriendid, function(err, user) {
-        frdDetails.push(user.user.name + ', ' + user.user.address);
-        callback();
-       });
-      }
-     },
-     function(err) {
-      response.render('profile.html', {
-       user: request.user,
-       friends: frdDetails
-      });
-     }
-    );
-   } else {
-    res.send(JSON.stringify(err), {
-     'Content-Type': 'application/json'
-    }, 404);
-   }
-  });
-
- });
-
- app.get('/search_member', function(req, res) {
-  var regex = new RegExp(req.query["term"], 'i');
-
-  var query = User.find({
-   $and: [{
-    'user.name': regex
-   }, {
-    _id: {
-     $ne: req.user._id
-    }
-   }]
-  }).limit(20);
-
-  // Execute query in a callback and return users list
-  query.exec(function(err, users) {
-   if (!err) {
-    // Method to construct the json result set
-
-    res.send(users, {
-     'Content-Type': 'application/json'
-    }, 200);
-   } else {
-    res.send(JSON.stringify(err), {
-     'Content-Type': 'application/json'
-    }, 404);
-   }
-  });
- });
-
- app.post('/friend', function(request, response) {
-  Friend.findOne({
-   $and: [{
-    'friend.mainfriendid': request.param('mainfriendid')
-   }, {
-    'friend.anotherfriendid': request.param('anotherfriendid')
-   }]
-  }, function(err, friend) {
-   if (err) {
-    return done(err);
-   }
-   if (friend) {
-    response.redirect('/profile');
-
-   } else {
-    if (request.param('anotherfriendid') != '') {
-     var newFriend = new Friend();
-     newFriend.friend.mainfriendid = request.param('mainfriendid');
-     newFriend.friend.anotherfriendid = request.param('anotherfriendid');
-     newFriend.save();
-    }
-    response.redirect('/profile');
-   }
-  });
- });
-
-
 
  // GET /auth/facebook
  // Use passport.authenticate() as route middleware to authenticate the
