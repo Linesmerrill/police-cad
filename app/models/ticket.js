aa3303afe6854ea44a762cacb4c717c527937ce1
Merrill Lines <linesmerrill@gmail.com>
2019-12-22 18:57:59 -0700

    fixes crazy search query with db

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -22,15 +22,22 @@ var ticketSchema = mongoose.Schema({
 });
 
 ticketSchema.methods.updateTicket = function (request, response) {
+  var adjustedNameAndDOB
   rawNameAndDOB = request.body.civFirstName
-  adjustedNameAndDOB = rawNameAndDOB.split(" | ")
-  firstNameAndLastName = adjustedNameAndDOB[0].split(" ")
+  if (rawNameAndDOB.includes("|")) {
+    adjustedNameAndDOB = rawNameAndDOB.split(" | ")
+    firstNameAndLastName = adjustedNameAndDOB[0].split(" ")
+    this.ticket.civFirstName = firstNameAndLastName[0].trim().charAt(0).toUpperCase() + firstNameAndLastName[0].trim().slice(1);
+    this.ticket.civLastName = firstNameAndLastName[1].trim().charAt(0).toUpperCase() + firstNameAndLastName[1].trim().slice(1);
+  } else {
+    this.ticket.civFirstName = request.body.civFirstName.trim().charAt(0).toUpperCase() + request.body.civFirstName.trim().slice(1);
+    this.ticket.civLastName = request.body.civLastName.trim().charAt(0).toUpperCase() + request.body.civLastName.trim().slice(1);
+  }
+  
 
   var additionalViolation = ""
   this.ticket.officerEmail = request.body.officerEmail;
   this.ticket.caseNumber = request.body.caseNumber;
-  this.ticket.civFirstName = firstNameAndLastName[0].trim().charAt(0).toUpperCase() + firstNameAndLastName[0].trim().slice(1);
-  this.ticket.civLastName = firstNameAndLastName[1].trim().charAt(0).toUpperCase() + firstNameAndLastName[1].trim().slice(1);
   this.ticket.plate = request.body.plate.trim().toUpperCase();
   this.ticket.model = request.body.model.trim().charAt(0).toUpperCase() + request.body.model.trim().slice(1);
   this.ticket.color = request.body.color.trim().charAt(0).toUpperCase() + request.body.color.trim().slice(1);
@@ -46,8 +53,10 @@ ticketSchema.methods.updateTicket = function (request, response) {
   if (request.body.otherInput !== '') {
     additionalViolation = ' - ' + additionalViolation + request.body.otherInput
   }
-  if (exists(adjustedNameAndDOB[2])) {
-    this.ticket.civID = adjustedNameAndDOB[2].trim();
+  if (exists(adjustedNameAndDOB)) {
+    if (exists(adjustedNameAndDOB[2])) {
+      this.ticket.civID = adjustedNameAndDOB[2].trim();
+    }
   }
   this.ticket.violation = request.body.violations + additionalViolation;
   this.ticket.amount = request.body.amount.trim();
