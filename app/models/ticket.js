3029d69d953ceed1c90e2a8f75300684aecf80c2
Merrill Lines <linesmerrill@gmail.com>
2020-01-27 08:40:10 -0700

    add null checks around all ticket trim methods

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -25,29 +25,47 @@ ticketSchema.methods.updateTicket = function (request, response) {
   //debug log showing the request body for the ticket request
   // console.debug("ticket request body: ", request.body)
   rawNameAndDOB = request.body.civFirstName
-  if (request.body.civFirstName.trim().length > 1 && request.body.civLastName.length > 1) {
-    this.ticket.civFirstName = request.body.civFirstName.trim().charAt(0).toUpperCase() + request.body.civFirstName.trim().slice(1);
-    this.ticket.civLastName = request.body.civLastName.trim().charAt(0).toUpperCase() + request.body.civLastName.trim().slice(1);
-  } else { 
-    console.error("cannot process empty values for civFirstName and civLastName");
+  if (exists(request.body.civFirstName) && exists(request.body.civLastName)){
+    if (request.body.civFirstName.trim().length > 1 && request.body.civLastName.length > 1) {
+      this.ticket.civFirstName = request.body.civFirstName.trim().charAt(0).toUpperCase() + request.body.civFirstName.trim().slice(1);
+      this.ticket.civLastName = request.body.civLastName.trim().charAt(0).toUpperCase() + request.body.civLastName.trim().slice(1);
+    } else { 
+      console.error("cannot process empty values for civFirstName and civLastName");
+      response.redirect('/police-dashboard');
+      return
+    }
+  }
+  else { 
+    console.error("cannot process null values for civFirstName and civLastName");
     response.redirect('/police-dashboard');
     return
   }
   
   this.ticket.officerEmail = request.body.officerEmail.toLowerCase();
   this.ticket.caseNumber = request.body.caseNumber;
-  this.ticket.plate = request.body.plate.trim().toUpperCase(); //optional
-  this.ticket.model = request.body.model.trim().charAt(0).toUpperCase() + request.body.model.trim().slice(1); //optional
-  this.ticket.color = request.body.color.trim().charAt(0).toUpperCase() + request.body.color.trim().slice(1); //optional
+  if (exists(request.body.plate)){
+    this.ticket.plate = request.body.plate.trim().toUpperCase(); //optional
+  }
+  if (exists(request.body.model)){
+    this.ticket.model = request.body.model.trim().charAt(0).toUpperCase() + request.body.model.trim().slice(1); //optional
+  }
+  if (exists(request.body.color)){
+    this.ticket.color = request.body.color.trim().charAt(0).toUpperCase() + request.body.color.trim().slice(1); //optional
+  }
   this.ticket.violation = request.body.fines;
-  this.ticket.amount = request.body.amount.trim();
-  this.ticket.date = request.body.date.trim();
-  this.ticket.time = request.body.time.trim();
+  if (exists(request.body.amount)){
+    this.ticket.amount = request.body.amount.trim();
+  }
+  if (exists(request.body.date)){
+    this.ticket.date = request.body.date.trim();
+
+  }
+  if (exists(request.body.time)){
+    this.ticket.time = request.body.time.trim();
+  }
   this.ticket.isWarning = request.body.isWarning;
   if (exists(request.body.civID)) {
     this.ticket.civID = request.body.civID.trim();
-  } else {
-    this.ticket.civID = request.body.civID
   }
   response.redirect('/police-dashboard');
 };
