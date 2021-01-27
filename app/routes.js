b1a1f166de5ded779435b66f6527782e56162cd7
Braeden57 <sowinskibraeden@gmail.com>
2021-01-26 21:01:28 -0800

    Bug Fixes/Improvements

--- app/routes.js
+++ app/routes.js
@@ -569,53 +569,58 @@ module.exports = function (app, passport, server) {
                     if (err) return console.error(err);
                     Call.find({
                       'call.communityID': req.user.user.activeCommunity,
-                    }, function (err, dbCalls) {
-                      let targetID;
+                    }, async function (err, dbCalls) {
+                      if (err) return console.error(err);
+                      let target;
+                      let allRegisteredVehicles = [];
                       for (let c=0; c<dbCivilians.length; c++) {
-                        targetID = dbCivilians[c].civilian.userID;
+                        target = `${dbCivilians[c].civilian.firstName} ${dbCivilians[c].civilian.lastName} | DOB: ${dbCivilians[c].civilian.birthday}`;
+                        await Vehicle.find({
+                          'vehicle.registeredOwner': target
+                        }, function (err, dbVehicles) {
+                          if (err) return console.error(err);
+                          for (let rc = 0; rc < dbVehicles.length; rc++) {
+                            allRegisteredVehicles.push(dbVehicles[rc]);
+                          }
+                        });
                       }
-                      Vehicle.find({
-                        'vehicle.userID': targetID
-                      }, function (err, dbVehicles) {
-                        if (req.user.user.activeCommunity == '' || req.user.user.activeCommunity == null) {
+                      if (req.user.user.activeCommunity == '' || req.user.user.activeCommunity == null) {
+                        return res.render('dispatch-dashboard', {
+                          user: req.user,
+                          vehicles: null,
+                          civilians: dbCivilians,
+                          registeredVehicles: allRegisteredVehicles,
+                          firearms: null,
+                          tickets: dbTickets,
+                          arrestReports: dbArrestReports,
+                          warrants: dbWarrants,
+                          communities: dbCommunities,
+                          bolos: dbBolos,
+                          calls: dbCalls,
+                          context: null
+                        });
+                      } else {
+                        User.find({
+                          'user.activeCommunity': req.user.user.activeCommunity
+                        }, function (err, dbCommUsers) {
+                          if (err) return console.error(err);
                           return res.render('dispatch-dashboard', {
                             user: req.user,
                             vehicles: null,
                             civilians: dbCivilians,
-                            registeredVehicles: dbVehicles,
+                            registeredVehicles: allRegisteredVehicles,
                             firearms: null,
                             tickets: dbTickets,
                             arrestReports: dbArrestReports,
                             warrants: dbWarrants,
                             communities: dbCommunities,
-                            commUsers: null,
+                            commUsers: dbCommUsers,
                             bolos: dbBolos,
                             calls: dbCalls,
                             context: null
                           });
-                        } else {
-                          User.find({
-                            'user.activeCommunity': req.user.user.activeCommunity
-                          }, function (err, dbCommUsers) {
-                            if (err) return console.error(err);
-                            return res.render('dispatch-dashboard', {
-                              user: req.user,
-                              vehicles: null,
-                              civilians: dbCivilians,
-                              registeredVehicles: dbVehicles,
-                              firearms: null,
-                              tickets: dbTickets,
-                              arrestReports: dbArrestReports,
-                              warrants: dbWarrants,
-                              communities: dbCommunities,
-                              commUsers: dbCommUsers,
-                              bolos: dbBolos,
-                              calls: dbCalls,
-                              context: null
-                            });
-                          });
-                        }
-                      });
+                        });
+                      }
                     });
                   });
                 })
@@ -660,54 +665,58 @@ module.exports = function (app, passport, server) {
                     if (err) return console.error(err);
                     Call.find({
                       'call.communityID': req.user.user.activeCommunity,
-                    }, function (err, dbCalls) {
+                    }, async function (err, dbCalls) {
                       if (err) return console.error(err);
-                      let targetID;
+                      let target;
+                      let allRegisteredVehicles = [];
                       for (let c=0; c<dbCivilians.length; c++) {
-                        targetID = dbCivilians[c].civilian.userID;
+                        target = `${dbCivilians[c].civilian.firstName} ${dbCivilians[c].civilian.lastName} | DOB: ${dbCivilians[c].civilian.birthday}`;
+                        await Vehicle.find({
+                          'vehicle.registeredOwner': target
+                        }, function (err, dbVehicles) {
+                          if (err) return console.error(err);
+                          for (let rc = 0; rc < dbVehicles.length; rc++) {
+                            allRegisteredVehicles.push(dbVehicles[rc]);
+                          }
+                        });
                       }
-                      Vehicle.find({
-                        'vehicle.userID': targetID
-                      }, function (err, dbVehicles) {
-                        if (req.user.user.activeCommunity == '' || req.user.user.activeCommunity == null) {
+                      if (req.user.user.activeCommunity == '' || req.user.user.activeCommunity == null) {
+                        return res.render('dispatch-dashboard', {
+                          user: req.user,
+                          vehicles: null,
+                          civilians: dbCivilians,
+                          registeredVehicles: allRegisteredVehicles,
+                          firearms: null,
+                          tickets: dbTickets,
+                          arrestReports: dbArrestReports,
+                          warrants: dbWarrants,
+                          communities: dbCommunities,
+                          bolos: dbBolos,
+                          calls: dbCalls,
+                          context: null
+                        });
+                      } else {
+                        User.find({
+                          'user.activeCommunity': req.user.user.activeCommunity
+                        }, function (err, dbCommUsers) {
+                          if (err) return console.error(err);
                           return res.render('dispatch-dashboard', {
                             user: req.user,
                             vehicles: null,
                             civilians: dbCivilians,
-                            registeredVehicles: dbVehicles,
+                            registeredVehicles: allRegisteredVehicles,
                             firearms: null,
                             tickets: dbTickets,
                             arrestReports: dbArrestReports,
                             warrants: dbWarrants,
                             communities: dbCommunities,
-                            commUsers: null,
+                            commUsers: dbCommUsers,
                             bolos: dbBolos,
                             calls: dbCalls,
                             context: null
                           });
-                        } else {
-                          User.find({
-                            'user.activeCommunity': req.user.user.activeCommunity
-                          }, function (err, dbCommUsers) {
-                            if (err) return console.error(err);
-                            return res.render('dispatch-dashboard', {
-                              user: req.user,
-                              vehicles: null,
-                              civilians: dbCivilians,
-                              registeredVehicles: dbVehicles,
-                              firearms: null,
-                              tickets: dbTickets,
-                              arrestReports: dbArrestReports,
-                              warrants: dbWarrants,
-                              communities: dbCommunities,
-                              commUsers: dbCommUsers,
-                              bolos: dbBolos,
-                              calls: dbCalls,
-                              context: null
-                            });
-                          });
-                        }
-                      });
+                        });
+                      }
                     });
                   });
                 });
@@ -767,30 +776,34 @@ module.exports = function (app, passport, server) {
                     if (err) return console.error(err);
                     Call.find({
                       'call.communityID': req.user.user.activeCommunity,
-                    }, function (err, dbCalls) {
+                    }, async function (err, dbCalls) {
                       if (err) return console.error(err);
-                      let targetID;
+                      let target;
+                      let allRegisteredVehicles = [];
                       for (let c=0; c<dbCivilians.length; c++) {
-                        targetID = dbCivilians[c].civilian.userID;
-                      }
-                      Vehicle.find({
-                        'vehicle.userID': targetID
-                      }, function (err, dbVehicles) {
-                        if (err) return console.error(err);
-                        return res.render('police-dashboard', {
-                          user: req.user,
-                          vehicles: null,
-                          civilians: dbCivilians,
-                          registeredVehicles: dbVehicles,
-                          firearms: null,
-                          tickets: dbTickets,
-                          arrestReports: dbArrestReports,
-                          warrants: dbWarrants,
-                          communities: dbCommunities,
-                          bolos: dbBolos,
-                          calls: dbCalls,
-                          context: null
+                        target = `${dbCivilians[c].civilian.firstName} ${dbCivilians[c].civilian.lastName} | DOB: ${dbCivilians[c].civilian.birthday}`;
+                        await Vehicle.find({
+                          'vehicle.registeredOwner': target
+                        }, function (err, dbVehicles) {
+                          if (err) return console.error(err);
+                          for (let rc = 0; rc < dbVehicles.length; rc++) {
+                            allRegisteredVehicles.push(dbVehicles[rc]);
+                          }
                         });
+                      }
+                      return res.render('police-dashboard', {
+                        user: req.user,
+                        vehicles: null,
+                        civilians: dbCivilians,
+                        registeredVehicles: allRegisteredVehicles,
+                        firearms: null,
+                        tickets: dbTickets,
+                        arrestReports: dbArrestReports,
+                        warrants: dbWarrants,
+                        communities: dbCommunities,
+                        bolos: dbBolos,
+                        calls: dbCalls,
+                        context: null
                       });
                     });
                   });
@@ -836,30 +849,34 @@ module.exports = function (app, passport, server) {
                     if (err) return console.error(err);
                     Call.find({
                       'call.communityID': req.user.user.activeCommunity,
-                    }, function (err, dbCalls) {
+                    }, async function (err, dbCalls) {
                       if (err) return console.error(err);
-                      let targetID;
+                      let target;
+                      let allRegisteredVehicles = [];
                       for (let c=0; c<dbCivilians.length; c++) {
-                        targetID = dbCivilians[c].civilian.userID;
-                      }
-                      Vehicle.find({
-                        'vehicle.userID': targetID
-                      }, function (err, dbVehicles) {
-                        if (err) return console.error(err);
-                        return res.render('police-dashboard', {
-                          user: req.user,
-                          vehicles: null,
-                          civilians: dbCivilians,
-                          registeredVehicles: dbVehicles,
-                          firearms: null,
-                          tickets: dbTickets,
-                          arrestReports: dbArrestReports,
-                          warrants: dbWarrants,
-                          communities: dbCommunities,
-                          bolos: dbBolos,
-                          calls: dbCalls,
-                          context: null
+                        target = dbCivilians[c].civilian.firstName + ' ' + dbCivilians[c].civilian.lastName + " | DOB: " + dbCivilians[c].civilian.birthday;
+                        await Vehicle.find({
+                          'vehicle.registeredOwner': target
+                        }, function (err, dbVehicles) {
+                          if (err) return console.error(err);
+                          for (let rc = 0; rc < dbVehicles.length; rc++) {
+                            allRegisteredVehicles.push(dbVehicles[rc]);
+                          }
                         });
+                      }
+                      return res.render('police-dashboard', {
+                        user: req.user,
+                        vehicles: null,
+                        civilians: dbCivilians,
+                        registeredVehicles: allRegisteredVehicles,
+                        firearms: null,
+                        tickets: dbTickets,
+                        arrestReports: dbArrestReports,
+                        warrants: dbWarrants,
+                        communities: dbCommunities,
+                        bolos: dbBolos,
+                        calls: dbCalls,
+                        context: null
                       });
                     });
                   });
