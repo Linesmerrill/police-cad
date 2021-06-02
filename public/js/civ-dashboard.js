77d1d601b21e9ff69ced85b0fe7c743f65617fee
Merrill Lines <linesmerrill@gmail.com>
2021-06-01 22:06:54 -0700

    revert back

--- public/js/civ-dashboard.js
+++ public/js/civ-dashboard.js
@@ -848,7 +848,7 @@ function updateUserBtnValue(value) {
 /* function to send socket when a civilian is updated/deleted. 
 This is to move away from reloading the page on civilian updates/deletions */
 $('#update-delete-civ-form').submit(function (e) {
-  // e.preventDefault(); //prevents page from reloading
+  e.preventDefault(); //prevents page from reloading
   var submitter_btn = $('#userBtnValue').val();
   console.log("submitter_btn: ", submitter_btn)
   var socket = io();
@@ -911,5 +911,5 @@ $('#update-delete-civ-form').submit(function (e) {
   //reset the form after form submit
   $('#update-delete-civ-form').trigger("reset");
   hideModal('viewCiv')
-  return false;
+  return true;
 })
