f6413bb3118a85e5f073e22a1217fa1557f7c14b
Merrill Lines <linesmerrill@gmail.com>
2022-01-13 10:22:17 -0700

    check for undefined values when inserting into emsVehicles table

--- app/models/emsVehicle.js
+++ app/models/emsVehicle.js
@@ -20,18 +20,36 @@ var emsVehicleSchema = mongoose.Schema({
 });
 
 emsVehicleSchema.methods.createVeh = function (req, res) {
-    this.emsVehicle.plate = req.body.plate.trim().toUpperCase();
-    this.emsVehicle.model = req.body.model.trim();
-    this.emsVehicle.engineNumber = req.body.engineNumber.trim();
-    this.emsVehicle.color = req.body.color.trim();
+    if (exists(req.body.plate)) {
+        this.emsVehicle.plate = req.body.plate.trim().toUpperCase();
+    }
+    if (exists(req.body.model)) {
+        this.emsVehicle.model = req.body.model.trim();
+    }
+    if (exists(req.body.engineNumber)) {
+        this.emsVehicle.engineNumber = req.body.engineNumber.trim();
+    }
+    if (exists(req.body.color)) {
+        this.emsVehicle.color = req.body.color.trim();
+    }
     this.emsVehicle.dispatchStatus = '10-41';
     this.emsVehicle.dispatchStatusSetBy = 'System';
     this.emsVehicle.dispatchOnDuty = true;
-    this.emsVehicle.registeredOwner = req.body.registeredOwner.trim();
+    if (exists(req.body.registeredOwner)) {
+        this.emsVehicle.registeredOwner = req.body.registeredOwner.trim();
+    }
     this.emsVehicle.activeCommunityID = req.body.activeCommunityID; // we set this when submitting the from so it should not be null
     this.emsVehicle.userID = req.body.userID; // we set this when submitting the from so it should not be null
     this.emsVehicle.createdAt = new Date();
     res.redirect('/ems-dashboard');
 };
+
+function exists(v) {
+    if (v !== undefined) {
+        return true
+    } else {
+        return false
+    }
+}
 
 module.exports = mongoose.model('EmsVehicle', emsVehicleSchema);
