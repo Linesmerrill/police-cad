5a9c5390c10afa66a15792bf6b56b98ec88c510c
Merrill Lines <linesmerrill@gmail.com>
2022-02-08 19:48:10 -0700

    update release page; update notification modal and badge

--- public/js/civ-dashboard.js
+++ public/js/civ-dashboard.js
@@ -312,7 +312,7 @@ function toggleInput(showClass, hideClass) {
 }
 
 function markAsRead() {
-  document.cookie = "notification-symbol=v2.30.0";
+  document.cookie = "notification-symbol=v2.31.0";
   $('#notification-symbol').removeClass('notif');
   $('#notification-count').text('');
 }
